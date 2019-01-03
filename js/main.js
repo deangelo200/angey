17fb7f6e7f78ebd06795572200273bf7d45014db
deangelo200 <deangeloalmeus@gmail.com>
2019-01-03 00:57:10 -0500

    First api

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+
+
+
+    function convertMusic() {
+
+        var url = document.getElementById('urlinput').value;
+        var apIUrl = "http://muzillamp3.com:8000/convert/"
+
+        $.post({
+            url: apIUrl,
+            data: {
+                url: url
+            },
+            success: function (response) {
+                title = response.title;
+                thumbnail = response.thumbnail;
+                $('.videoTitle').append(title);
+                $('.videoThumbnail').attr("src", thumbnail);
+
+            }
+        });
+
+    }
+
+
+
+
+    $(".convertButton").click(function () {
+
+        convertMusic();
+    });
+
+
+
+
+
+
+
+
+
+});
