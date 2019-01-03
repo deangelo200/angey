da083f8f10e3feb5a5da31874762c32f4c2ac9ab
deangelo200 <deangeloalmeus@gmail.com>
2019-01-03 01:19:56 -0500

    device test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-$(document).ready(function () {
 
+document.addEventListener("deviceready", onDeviceReady, false);
 
-
-
+function onDeviceReady() {
+        
+   
     function convertMusic() {
 
         var url = document.getElementById('urlinput').value;
@@ -33,11 +34,55 @@ $(document).ready(function () {
     });
 
 
+ 
+        
+
+    
+}
 
 
 
 
-
-
-
-});
+//$(document).ready(function () {
+//
+//
+//
+//
+//    function convertMusic() {
+//
+//        var url = document.getElementById('urlinput').value;
+//        var apIUrl = "http://muzillamp3.com:8000/convert/"
+//
+//        $.post({
+//            url: apIUrl,
+//            data: {
+//                url: url
+//            },
+//            success: function (response) {
+//                title = response.title;
+//                thumbnail = response.thumbnail;
+//                $('.videoTitle').append(title);
+//                $('.videoThumbnail').attr("src", thumbnail);
+//
+//            }
+//        });
+//
+//    }
+//
+//
+//
+//
+//    $(".convertButton").click(function () {
+//
+//        convertMusic();
+//    });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//});
