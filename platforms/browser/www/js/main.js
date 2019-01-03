9b4581ec8c630d56c9446187775c359b4ac73551
deangelo200 <deangeloalmeus@gmail.com>
2019-01-03 09:58:17 -0500

    big upgrade lol framework7

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,28 @@
+    function convertMusic() {
+
+        var url = document.getElementById('urlinput').value;
+        var apIUrl = "http://muzillamp3.com:8000/convert/"
+
+        $.post({
+            url: apIUrl,
+            data: {
+                url: url
+            },
+            success: function (response) {
+                title = response.title;
+                thumbnail = response.thumbnail;
+                $('.videoTitle').append(title);
+                $('.videoThumbnail').attr("src", thumbnail);
+
+            }
+        });
+
+    }
+
+
+
+
+    $(".convertButton").click(function () {
+
+        convertMusic();
+    });
